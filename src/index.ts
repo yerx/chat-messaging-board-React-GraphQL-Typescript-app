44c417d8fd42d0a83a37cff0ecde3da7cf57938a
yerx <yer.x.xiong@gmail.com>
2020-09-02 09:24:35 -0400

    set up microrm to post to the database and retrieve posts

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,15 @@ import microConfig from "./mikro-orm.config";
 
 // create async main function
 const main = async () => {
+  // connect to the database
   const orm = await MikroORM.init(microConfig);
-
-  const post = orm.em.create(Post, { title: "my first post" });
-  await orm.em.persistAndFlush(post);
+  // run migrations
+  await orm.getMigrator().up();
+  // run sql
+  // const post = orm.em.create(Post, { title: "my first post" });
+  // await orm.em.persistAndFlush(post);
+  const posts = await orm.em.find(Post, {});
+  console.log(posts);
 };
 
 main().catch((err) => {
